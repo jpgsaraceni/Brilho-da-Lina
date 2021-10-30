5e5dad9af88fa1da01f57ccf4703507c977d5c37
jpgsaraceni <jpgomes@id.uff.br>
2021-10-30 12:15:23 -0300

    Add link to game

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ const writeButton = document.getElementById('write-btn');
 const writeModal = document.getElementById('textarea-modal');
 const closeWriteModalBtn = document.getElementById('close-textarea-modal');
 
-writeButton.onclick = () => writeModal.style.visibility = 'visible';
+const gamesButton = document.getElementById('games-btn');
+const gamesModal = document.getElementById('games-modal');
+const closeGamesModalBtn = document.getElementById('close-games-modal');
+
+writeButton.onclick = () => {
+  writeModal.style.visibility = 'visible'
+  gamesModal.style.visibility = 'hidden';
+};
 closeWriteModalBtn.onclick = () => writeModal.style.visibility = 'hidden';
+
+gamesButton.onclick = () => {
+  gamesModal.style.visibility = 'visible'
+  writeModal.style.visibility = 'hidden';
+};
+closeGamesModalBtn.onclick = () => gamesModal.style.visibility = 'hidden';
