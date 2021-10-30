93154ce3965de5fa703e37b526b0c0420537d6a1
jpgsaraceni <jpgomes@id.uff.br>
2021-10-30 12:06:27 -0300

    Limit textarea size and add close button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const writeButton = document.getElementById('write-btn');
 const writeModal = document.getElementById('textarea-modal');
+const closeWriteModalBtn = document.getElementById('close-textarea-modal');
 
 writeButton.onclick = () => writeModal.style.visibility = 'visible';
+closeWriteModalBtn.onclick = () => writeModal.style.visibility = 'hidden';
